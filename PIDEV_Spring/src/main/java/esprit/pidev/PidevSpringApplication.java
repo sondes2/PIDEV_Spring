2518329f52fe7465aa6428eebb44a23001b13ab7
DESKTOP-LH3S4JH\mortadha <mortadha.bouallagui@esprit.tn>
2021-02-28 15:15:43 +0100

    First Commit

---
+++ PIDEV_Spring/src/main/java/esprit/pidev/PidevSpringApplication.java
@@ -0,0 +1,13 @@
+package esprit.pidev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PidevSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PidevSpringApplication.class, args);
+	}
+
+}
---
+++ PIDEV_Spring/src/test/java/esprit/pidev/PidevSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package esprit.pidev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PidevSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
